90b5a575855d9fb285d82fb44c334a86498ebf6d
maykiscofield <ozgurkuzun0@gmail.com>
2026-06-09 13:50:27 +0300

    use cloudflare workers deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({
-      server: {
-        preset: "vercel",
-      },
-    }),
+    tanstackStart(),
     tsconfigPaths(),
   ],
 });
