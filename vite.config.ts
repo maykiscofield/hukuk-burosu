c45b4ec9ad284278105bed192f0ab45725cfde54
maykiscofield <ozgurkuzun0@gmail.com>
2026-06-09 14:34:41 +0300

    temiz

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/", 
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
