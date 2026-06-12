edbb3820ab49e4bd3faf3372c1ce7612475d294f
maykiscofield <ozgurkuzun0@gmail.com>
2026-06-12 14:18:32 +0300

    fix tailwind css plugin and remove dead ssr server

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/", 
+  base: "/",
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     react(),
+    tailwindcss(),
     tsconfigPaths(),
   ],
   build: {
